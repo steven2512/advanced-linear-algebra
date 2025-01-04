0b783e5954f7954727f7b92f3d90426373c0cf87
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-05 09:35:07 +1100

    Added generic Matrix Class defining a simple m x n mathematical matrix

---
+++ matrix-operations/matrix.py
@@ -0,0 +1,6 @@
+class Matrix:
+    """A class represents a mathematical matrix defined by m columns and n rows"""
+    def __init__(self, content, columns, rows):
+        self.content = content
+        self.columns = columns
+        self.rows = rows
