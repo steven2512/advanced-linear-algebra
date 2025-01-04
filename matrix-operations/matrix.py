7644d38733b2f405129bf9941abcf075dc77713f
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-05 10:02:26 +1100

    Added is_valid method to validate a mxn Matrix Object and special methods __len__ and __str__

--- matrix-operations/matrix.py
+++ matrix-operations/matrix.py
@@ -1,6 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Generic
+from ctypes import py_object
+from typing import TypeVar, Generic
+
 class Matrix:
     """A class represents a mathematical matrix defined by m columns and n rows"""
-    def __init__(self, content, columns, rows):
-        self.content = content
+    def __init__(self, content=[], columns=0, rows=0):
+        self.content = content 
         self.columns = columns
         self.rows = rows
+    def is_valid(self):
+        """Return a Boolean to confirm validity of a matrix"""
+        if len(self.content) // self.columns == 0 and len(self.content) // self.columns == self.rows:
+           return True
+        return False 
+    def __len__(self):
+        """Total entries of the matrix"""
+        return len(self.content)
+    def __str__(self):
+        return f"{self.rows} x {self.columns} matrix"
+
