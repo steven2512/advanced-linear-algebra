ff3395f99ef97ab7ca0292714216d782da6eb622
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-09 12:15:44 +1100

    Finalized subtraction module used for matrix A and B

--- matrix-operations/subtraction.py
+++ matrix-operations/subtraction.py
@@ -2,5 +2,15 @@ from matrix import Matrix
 from validity import sub_add_check
 
 def subtraction(A: Matrix, B: Matrix):
+    """Mathematical Matrix Subtraction A - B"""
     if not sub_add_check(A, B):
         return
+    
+    final_matrix_content = []
+    for i in range(len(A)):
+        final_matrix_content.append(A.get_content()[i]-B.get_content()[i])
+    return Matrix(
+                content = final_matrix_content, 
+                rows = A.get_rows(), 
+                columns = A.get_columns()   
+                  )
