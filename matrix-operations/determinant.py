6c6f5aa42f920c511e564b24e1e8509173322c27
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-11 10:26:48 +1100

    Finished 3x3 Matrix Determinant case for determinant.py module

--- matrix-operations/determinant.py
+++ matrix-operations/determinant.py
@@ -8,6 +8,22 @@ def determinant(A: Matrix):
     if not A.is_square():
         raise ValueError("Non-square Matrix does not have a determinant")
     
+    content = A.get_content()
+
+    det = 0
+
     #2x2 case
     if A.get_rows() == 2 and A.get_columns() == 2:
-        return A.get_content()[0] * A.get_content()[3] - A.get_content()[1] * A.get_content()[2]
+        det = (content[0] * content[3]) - (content[1] * content[2])
+
+    #3x3 case
+    if A.get_rows() == 3 and A.get_columns() == 3:
+        det = content[0] * (content[4] * content[8] - content[5] * content[7]) \
+        - content[1] * (content[3] * content[8] - content[5] * content[6]) \
+        + content[2] * (content[3] * content[7] - content[4] * content[6])
+    
+    return det
+
+#3x3 Test Case
+A = Matrix(content = [1,12,3,4,5,6,7,8,9], rows = 3, columns = 3)
+print(determinant(A))
