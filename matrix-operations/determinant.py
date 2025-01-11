2d7be1a78dafda515d9eb3aa9ab0a7f294be15e3
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-11 11:07:33 +1100

    Initialized matrix-decomposition package

--- matrix-operations/determinant.py
+++ matrix-operations/determinant.py
@@ -22,8 +22,16 @@ def determinant(A: Matrix):
         - content[1] * (content[3] * content[8] - content[5] * content[6]) \
         + content[2] * (content[3] * content[7] - content[4] * content[6])
     
+    #nxn case
+
     return det
 
+
+
+#2x2 Test Case
+# A = Matrix(content = [1,12,3,4], rows = 2, columns = 2)
+# print(determinant(A))
+
 #3x3 Test Case
-A = Matrix(content = [1,12,3,4,5,6,7,8,9], rows = 3, columns = 3)
-print(determinant(A))
+# A = Matrix(content = [1,12,3,4,5,6,7,8,9], rows = 3, columns = 3)
+# print(determinant(A))
