6035968b5b5d582d46659f3857cb4a3aa326cb0d
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-12 11:35:24 +1100

    Fundamentally finished standard case of filling up L and U matrices inclusive of efficient diagonals filling on-the-fly

--- matrix_decompositions/lu.py
+++ matrix_decompositions/lu.py
@@ -2,17 +2,31 @@ from matrix_operations.matrix import Matrix
 
 def lu(A: Matrix):
     A.is_valid()
+    content = A.get_content()
 
     row = A.get_rows()
     col = A.get_columns()
     iters = row - 1
 
-    content_l = []
-    content_u = []
+    content_l = [0] * len(A)
+    content_u = [0] * (col**2)
 
     for i in range(iters):
         under_pivot = iters - i
+        pivot_idx = col * i + i
+
+        #Fill up L and U diagonal's with pivots and 1's on the go for efficiency
+        content_l[pivot_idx] = 1
+        content_u[pivot_idx] = content[pivot_idx]
+        
+        # if content[pivot_idx] == 0:
+        #     pivoting()
         for j in range(under_pivot):
-            index = col * (j+1) + i
+            k = col * (j+1) + i
+            mp = content[k] / content[pivot_idx] 
+            content_l[k] = mp
+            content_u[k] = 0
+            
+            
 
 
