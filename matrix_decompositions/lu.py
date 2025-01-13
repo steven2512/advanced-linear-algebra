4a6af9a364b5e9a9f271fc73c23017d48d605ba4
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-13 16:44:59 +1100

    Fixed the under pivot issues, however, figuring out spanning the rest of the columns for updating row Rn

--- matrix_decompositions/lu.py
+++ matrix_decompositions/lu.py
@@ -12,25 +12,32 @@ def lu(A: Matrix):
     iters = row - 1
 
     content_l = [0] * len(A)
-    content_u = [0] * (col**2)
+    content_u = content
 
 
     for i in range(iters):
         under_pivot = iters - i
-        pivot_idx = col * i + i
-
+        pivot_row = col * i
+        pivot_idx = pivot_row + i
+        
         #Fill up L and U diagonal's with pivots and 1's on the go for efficiency
         content_l[pivot_idx] = 1
-        content_u[pivot_idx] = content[pivot_idx]
         print(f"Pivot index is {pivot_idx}")
         # if content[pivot_idx] == 0:
         #     pivoting()
         for j in range(under_pivot):
-            k = col * (j+i+1) + i
-            mp = content[k] / content[pivot_idx] 
-            content_l[k] = mp
-            content_u[k] = 0
-            print(f"Index k is {k}")
+            start_idx = col * (i+j+1)
+            mp = content_u[start_idx] / content_u[pivot_idx]
+
+            content_u[start_idx] = 0
+            content_l[start_idx] = mp
+            
+            for k in range(1, col-i-1): 
+                index = start_idx + k          
+                #Update U matrix
+                content_u[index] = content_u[index] - mp * content_u[pivot_idx+k]
+
+            
 
     #Fill up the last pivot positions in L and U
     content_l[col * (row-1) + (row-1)] = 1
@@ -44,6 +51,7 @@ def lu(A: Matrix):
 A = Matrix(content = [2,1,4,5,3,2,1,2,1], rows = 3, columns = 3)
 
 l,u = lu(A)
+print(f"Matrix A is {A}")
 print(f"Matrix L is {l}")
 print(f"Matrix U is {u}")
 print(f"Matrix LU is equal to {multiplication(l,u)}")
