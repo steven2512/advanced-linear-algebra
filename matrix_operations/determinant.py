305e7dc922f6bf2f4903a07ba24c56fc86f2b1b8
Nguyen <nguyenduong251202@gmail.com>
2025-01-14 10:02:11 +1100

    Removed redundant determinant of L calculation in det(A) = det(L) * det(U) since L is a unit lower triangular vector where det(L) = 1

--- matrix_operations/determinant.py
+++ matrix_operations/determinant.py
@@ -24,15 +24,19 @@ def determinant(A: Matrix):
         + content[2] * (content[3] * content[7] - content[4] * content[6])
     
     #nxn case
+    #We decompose any matrix A into a lower triagnular matrix L and uper triangular matrix U, so we have det(A) = det(L) * det(U) (determinant multiplicativity)
 
-    #Use LU decomposition
     l,u = lu(A)
+    print(l, u)
+
 
     det = 1
-
     for i in range(A.get_rows()):
         index = A.get_columns() * i + i
-        det *= l.get_content()[index] * u.get_content()[index]
+
+        #Determinant of a lower triangular/upper triangular matrix is the product of its diagonal
+        #Here we skip calculating determinant of L because L diagonal's is all 1's
+        det *= u.get_content()[index]
 
     return det
 
@@ -47,5 +51,5 @@ def determinant(A: Matrix):
 # print(determinant(A))
 
 #nxn case
-A = Matrix(content = [1,12,3,4,5,6,7,8,9,10,11,12,14,25,30,32], rows = 4, columns = 4)
-print(determinant(A))
+# A = Matrix(content = [1,12,3,4,5,6,7,8,9,10,11,12,14,25,30,32], rows = 4, columns = 4)
+# print(determinant(A))
