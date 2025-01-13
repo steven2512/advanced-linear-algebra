b51c61b34d2be1cac7544e0a08fac3d23bf21bec
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-14 10:35:20 +1100

    Added the 2x2 case for inverse of a Matrix in inverse.py module

--- matrix_operations/inverse.py
+++ matrix_operations/inverse.py
@@ -1,1 +1,28 @@
 from matrix_operations.matrix import Matrix
+from matrix_operations.determinant import determinant
+from matrix_decompositions.lu import lu
+
+
+def inverse(A: Matrix):
+    A.is_valid()
+
+    det = determinant(A)
+    if det == 0:
+        raise ValueError("Matrix with determinant = 0 does not have an inverse")
+    row = A.get_rows()
+    col = A.get_columns()
+    cont = A.get_content()
+
+    inverse = None
+
+    #2x2 case
+    if row == 2 and col == 2:
+        inv_cont = [cont[3], -cont[1], -cont[2], cont[0]]
+        inverse = Matrix(content = inv_cont,
+                         rows = 2,
+                         columns = 2)
+
+
+    return inverse        
+
+    
