4c46969aa5e52a44e7512defd29d4b77cbfd3c70
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-16 12:25:01 +1100

    Fundamentally finish high-level representations of a Mathematical Term Class in term.py module

--- system_of_linear_equations/term.py
+++ system_of_linear_equations/term.py
@@ -1,6 +1,15 @@
+from system_of_linear_equations.variable import Variable
+
 class Term:
-    "Represnts a mathematical term (inclusive or exclusive of variables)"
-    def __init__(self, coefficient: int, variables: list):
+    "Represents a mathematical term (inclusive or exclusive of variables)"
+    def __init__(self, coefficient: int, variables: list[Variable]):
         self.coefficient = coefficient
         self.variables = variables
+    def get_variable(self, variable: str):
+        if len(self.variables) == 1:
+            return self.variables[0]
+        else:
+            for var in self.variables:
+                if var.get_symbol() == variable:
+                    return var
     
--- system_of_linear_equations/variable.py
+++ system_of_linear_equations/variable.py
@@ -1,8 +1,10 @@
 class Variable:
     """Represents standard Mathematical Variables used in equations"""
-    def __init__(self, symbol):
+    def __init__(self, symbol: str):
         self.value = None
         self.symbol = symbol
+    def get_symbol(self):
+        return self.symbol
     def set_value(self, value):
         self.value = value
     def __str__(self):
