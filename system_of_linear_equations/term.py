34fe0783b0eb17d7863e3acb240c9c517e17179a
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-17 09:08:15 +1100

    Changed Term Class to only support 1 variable for the time being, for equation testing purposes

--- system_of_linear_equations/term.py
+++ system_of_linear_equations/term.py
@@ -2,14 +2,13 @@ from system_of_linear_equations.variable import Variable
 
 class Term:
     "Represents a mathematical term (inclusive or exclusive of variables)"
-    def __init__(self, coefficient: int, variables: list[Variable]):
+    def __init__(self, coefficient: int, variable: str = None):
         self.coefficient = coefficient
-        self.variables = variables
-    def get_variable(self, variable: str):
-        if len(self.variables) == 1:
-            return self.variables[0]
-        else:
-            for var in self.variables:
-                if var.get_symbol() == variable:
-                    return var
+        self.variable = variable
+    def get_variable(self):
+        if self.variable is None:
+            raise ValueError(f"This term has no variable")
+    def get_coefficent(self):
+        return self.coefficient
+
     
