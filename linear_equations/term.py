2d48aac94d5f17998a7767e2f5d4b9d0d7c7fdbd
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-17 09:20:42 +1100

    Initialized Polynomial class for representing a Mathematical Polynomial

--- linear_equations/term.py
+++ linear_equations/term.py
@@ -1,8 +1,8 @@
-from system_of_linear_equations.variable import Variable
+from linear_equations.variable import Variable
 
 class Term:
     "Represents a mathematical term (inclusive or exclusive of variables)"
-    def __init__(self, coefficient: int, variable: str = None):
+    def __init__(self, coefficient: int = 1, variable: Variable = None):
         self.coefficient = coefficient
         self.variable = variable
     def get_variable(self):
@@ -10,5 +10,5 @@ class Term:
             raise ValueError(f"This term has no variable")
     def get_coefficent(self):
         return self.coefficient
-
-    
+    def __str__(self):
+        return f"Term {self.coefficient}{self.variable if self.variable is not None else ''}"
