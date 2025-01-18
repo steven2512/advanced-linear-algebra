18ac2973dc5b6c24b3bc3c61196bde1ef00bd056
Nguyen Duong <nguyenduong251202@gmail.com>
2025-01-18 13:25:49 +1100

    Removed operators parameter in Polynomial and improved scalability of future equations

--- linear_equations/polynomial.py
+++ linear_equations/polynomial.py
@@ -5,37 +5,33 @@ import pdb
 
 class Polynomial:
 
-    def __init__(self, terms: list[Term], operators: list[Operator] = []):
-        if len(operators) != len(terms) - 1:
-            raise ValueError("The number of Terms does not have the required number of Operators (operators != terms - 1)")
-        
+    def __init__(self, terms: list[Term] = []):
         self.terms = terms
-        self.operators = operators
-
 
     def get_terms(self):
         return self.terms
 
     def add_term(self, term: Term):
-        self.terms.append(term)
+            self.terms.append(term)
 
     def __str__(self):
         final = ''
-        for i in range(len(self.terms)):
-            final+=str(self.terms[i])
-            if i<len(self.terms)-1:
-                final+=str(self.operators[i])
+        for term in self.terms:
+            if term.get_coefficent() > 0:
+                final+='+'+str(term)
+            else:
+                final+=str(term)
         return final
 
 
-# #Test
-# var1 = Variable('x')
-# var2 = Variable('y')
-# term1 = Term(2, var1)
-# term2 = Term(4, var2)
-# term3 = Term(7)
-# pol = Polynomial([term1, term2, term3], [Operator('+'), Operator("-")])
-# print(pol)
+#Test
+var1 = Variable('x')
+var2 = Variable('y')
+term1 = Term(-2, var1)
+term2 = Term(4, var2)
+term3 = Term(-7)
+pol = Polynomial([term1, term2, term3])
+print(pol)
 
 
         
--- linear_equations/solve.py
+++ linear_equations/solve.py
@@ -1,5 +1,6 @@
 from linear_equations.equation import Equation
 from linear_equations.term import Term
+from linear_equations.polynomial import Polynomial
 
 def solve(equation: Equation, variable: str):
     lhs = equation.get_lhs()
@@ -10,7 +11,7 @@ def solve(equation: Equation, variable: str):
     
 
     new_lhs = 0
-    new_rhs = Term()
+    new_rhs = Polynomial()
 
     if variable not in term_lhs and variable not in term_rhs:
         raise ValueError(f"Variable {variable} does not exist in the equation")
@@ -21,5 +22,5 @@ def solve(equation: Equation, variable: str):
         if term.get_variable() == variable:
             main_term = term
         else:
-            new_rhs.add
+            new_rhs.add_term(term, )
 
