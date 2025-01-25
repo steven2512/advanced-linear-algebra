7ceeb1f699cd772c084b6021c3e6fc945ef7a4b0
Nguyen <nguyenduong251202@gmail.com>
2025-01-25 16:44:15 +1100

    Finished dot.py module for calculating dot product

---
+++ vector_operations/dot.py
@@ -0,0 +1,14 @@
+from matrix_operations.matrix import Matrix
+
+def dot(v: Matrix, q: Matrix):
+    v.is_valid()
+    q.is_valid()
+    if not v.is_vector():
+        raise ValueError(f"v or q is not a valid vector")
+    elif len(v) != len(q):
+        raise ValueError(f"v and q does have equal dimensions")
+    dot = 0
+    for i in range(v.get_content()):
+        dot += v.get_content()[i] * q.get_content()[i]
+
+    return dot
